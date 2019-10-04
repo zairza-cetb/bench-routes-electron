48e85596b4778476e46c0ea884706cb3444a5413
Harkishen-Singh <harkishensingh@hotmail.com>
2019-10-04 23:15:42 +0530

    support for notifications

--- app/renderer/processes/sockets.ts
+++ app/renderer/processes/sockets.ts
@@ -12,9 +12,35 @@ export class Sockets {
         this.Socket = new WebSocket(this.SocketURL);
     }
 
-    sendMessage(message: Message) {
+    /**
+     * Sends message synchronously to the service
+     * @param message => message text to be sennt to the backend service
+     */
+    sendMessage(message: Message): void {
         this.Socket.send(message.toString());
     }
 
-    
+    /**
+     * Closes the Socket connection with the service
+     */
+    close(): void {
+        this.Socket.close();
+    }
+
+    /**
+     * Closes the Socket connection with the service
+     */
+    connect(): void {
+        this.Socket.OPEN;
+    }
+
+    /**
+     * Returns a socket object.
+     * @returns a websocket connection instance.
+     */
+    get(): WebSocket {
+        return this.Socket;
+    }
+
+
 }
