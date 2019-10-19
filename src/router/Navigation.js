c7d77c04c79487bc25821a56967353856bdecaf2
dib6596 <cory.harkins@cerecore.net>
2019-10-19 12:54:05 -0500

    Add Settings Route and fix CSS Layout issues
    Signed-off-by: dib6596 <cory.harkins@cerecore.net>

--- src/router/Navigation.js
+++ src/router/Navigation.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import Dashboard from '../components/dashboard/Dashboard';
-import Monitoring from '../components/monitoring/Monitoring';
-import Benchmarks from '../components/benchmarks/Benchmarks';
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "../components/dashboard/Dashboard";
+import Monitoring from "../components/monitoring/Monitoring";
+import Benchmarks from "../components/benchmarks/Benchmarks";
+import Settings from "../components/settings/Settings";
 
 export default class Navigator extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path='/' component={Dashboard} />
-          <Route path='/monitoring' component={Monitoring} />
-          <Route path='/benchmarks' component={Benchmarks} />
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/monitoring" component={Monitoring} />
+          <Route path="/benchmarks" component={Benchmarks} />
+          <Route path="/settings" component={Settings} />
         </Switch>
       </Router>
     );
