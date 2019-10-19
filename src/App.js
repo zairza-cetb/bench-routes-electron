c77ab6e644553c1883563606c309a9dc0f6d20bc
Harkishen-Singh <harkishensingh@hotmail.com>
2019-10-20 01:20:09 +0530

    dev header and notifs
    Signed-off-by: dib6596 <cory.harkins@cerecore.net>

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/layouts/Header";
+import Sidebar from "./components/layouts/Sidebar";
+import Navigator from "./router/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar className="sidebar" />
+      <div className="inner-component">
+        <Header />
+        <Navigator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
