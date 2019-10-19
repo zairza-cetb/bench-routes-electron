5d783788ca826ec296803099225ad12d967e3419
dib6596 <cory.harkins@cerecore.net>
2019-10-19 12:54:05 -0500

    Add Settings Route and fix CSS Layout issues
    Signed-off-by: dib6596 <cory.harkins@cerecore.net>

--- src/router/Navigation.js
+++ src/router/Navigation.js
@@ -5,22 +5,6 @@ import Monitoring from "../components/monitoring/Monitoring";
 import Benchmarks from "../components/benchmarks/Benchmarks";
 import Settings from "../components/settings/Settings";
 
-<<<<<<< HEAD
-export default class Navigator extends React.Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/monitoring" component={Monitoring} />
-          <Route path="/benchmarks" component={Benchmarks} />
-          <Route path="/settings" component={Settings} />
-        </Switch>
-      </Router>
-    );
-  }
-}
-=======
 const Navigator = () => (
   <Router>
     <Switch>
@@ -32,4 +16,3 @@ const Navigator = () => (
 );
 
 export default Navigator;
->>>>>>> Eslint integration with airbnb extension (#12)
