35fb99aa006535a669595a441bd198d9a707625f
Harkishen-Singh <harkishensingh@hotmail.com>
2019-10-20 01:20:09 +0530

    dev header and notifs
    Signed-off-by: dib6596 <cory.harkins@cerecore.net>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import Header from './components/layouts/Header';
@@ -6,17 +7,30 @@ import Navigator from './router/Navigation';
 import Notification from './components/notification/Notification';
 
 global.showNotificationSection = true;
+=======
+import React from "react";
+import "./App.css";
+import Header from "./components/layouts/Header";
+import Sidebar from "./components/layouts/Sidebar";
+import Navigator from "./router/Navigation";
+>>>>>>> dev header and notifs
 
 function App() {
   return (
     <div className="App">
       <Sidebar className="sidebar" />
+<<<<<<< HEAD
       <div className="outer-wrapper">
         <Header />
         <Notification />
         <div className="inner-component">
           <Navigator />
         </div>
+=======
+      <div className="inner-component">
+        <Header />
+        <Navigator />
+>>>>>>> dev header and notifs
       </div>
     </div>
   );
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export default class Header extends React.Component {
-    render() {
-        return (
-            <header>
-                bench-routes
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+        <div className="logo-name">Bench-routes</div>
+
+        <div className="notification-icon">
+          <img src="assets/icons/notify-icon.svg" alt="notification" />
+        </div>
+      </header>
+    );
+  }
 }
