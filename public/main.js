10079b33e5f3d2f40ac0328fb1fb25559df49dd9
muskankhedia <muskan.khedia2000@gmail.com>
2019-10-20 14:14:53 +0530

    pull

--- public/main.js
+++ public/main.js
@@ -3,26 +3,16 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
-<<<<<<< HEAD
-const url = require('url');
-const isDev = require('electron-is-dev');
-
-=======
 const isDev = require('electron-is-dev');
 
 require('electron-reload')(__dirname);
 
->>>>>>> ce666b881d3686f1dbb4658577ad74ef1971963f
 let mainWindow;
 
 function createWindow() {
 
     mainWindow = new BrowserWindow({
-<<<<<<< HEAD
-        minWidth: 800,
-=======
         minWidth: 1000,
->>>>>>> ce666b881d3686f1dbb4658577ad74ef1971963f
         minHeight: 600,
         center: true,
         title: 'Bench-Routes - Mark your routes',
@@ -30,23 +20,13 @@ function createWindow() {
           nodeIntegration: true
         },
         hasShadow: true,
-<<<<<<< HEAD
-        autoHideMenuBar: true
-    })
-=======
         autoHideMenuBar: true,
         transparent: false
     });
->>>>>>> ce666b881d3686f1dbb4658577ad74ef1971963f
 
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
     mainWindow.webContents.openDevTools();
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> ce666b881d3686f1dbb4658577ad74ef1971963f
     mainWindow.on('closed', () => mainWindow = null);
 
 }
@@ -63,8 +43,4 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> ce666b881d3686f1dbb4658577ad74ef1971963f
