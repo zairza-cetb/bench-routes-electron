ec9888b343766c3d907dbc4c668c804185889fce
muskankhedia <muskan.khedia2000@gmail.com>
2019-10-27 00:58:22 +0530

    added submenu for benchmarks
    Signed-off-by: muskankhedia <muskan.khedia2000@gmail.com>

--- src/components/benchmarks/Benchmarks.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Benchmarks extends React.Component {
-  render() {
-    return <div>This is Benchmarking</div>;
-  }
-}
--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -3,64 +3,158 @@ import "./layouts.style.css";
 import { HashRouter as Router, Link } from "react-router-dom";
 
 export default class Sidebar extends React.Component<{}> {
-  render() {
-    return (
-      <Router>
-        <div className="sidebar">
-          <div className="sidebar-content">
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <div>
-                <div className="sidebar-inner">
-                  <img
-                    src="assets/icons/dashboard-icon.svg"
-                    className="sidebar-inner"
-                    alt="dashboard"
-                  />
-                  <div className="sidebar-head sidebar-inner">Dashboard</div>
+    state = {
+        showBenchmarkSubmenu: false
+    };
+
+    BenchmarkSubmenu = () => {
+        if (this.state.showBenchmarkSubmenu) {
+            this.setState({
+                showBenchmarkSubmenu: false
+            });
+        } else {
+            this.setState({
+                showBenchmarkSubmenu: true
+            });
+        }
+    };
+
+    render() {
+        return (
+            <Router>
+                <div className="sidebar">
+                    <div className="sidebar-content">
+                        <Link to="/" style={{ textDecoration: "none" }}>
+                            <div>
+                                <div className="sidebar-inner">
+                                    <img
+                                        src="assets/icons/dashboard-icon.svg"
+                                        className="sidebar-inner"
+                                        alt="dashboard"
+                                    />
+                                    <div className="sidebar-head sidebar-inner">
+                                        Dashboard
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                        <Link
+                            to="/monitoring"
+                            style={{ textDecoration: "none" }}
+                        >
+                            <div>
+                                <div className="sidebar-inner">
+                                    <img
+                                        src="assets/icons/monitoring-icon.svg"
+                                        className="sidebar-inner"
+                                        alt="monitoring"
+                                    />
+                                    <div className="sidebar-head sidebar-inner">
+                                        Monitoring
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <div
+                            onClick={() => {
+                                this.BenchmarkSubmenu();
+                            }}
+                        >
+                            <div className="sidebar-inner benchmarking">
+                                <img
+                                    src="assets/icons/bench-icon.svg"
+                                    className="sidebar-inner"
+                                    alt="Benchmarks"
+                                />
+                                <div className="sidebar-head sidebar-inner">
+                                    Benchmarks
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="benchmark-submenu">
+                            {this.state.showBenchmarkSubmenu ? (
+                                <div>
+                                    <Link
+                                        to="/ping"
+                                        style={{ textDecoration: "none" }}
+                                    >
+                                        <div>
+                                            <div className="sidebar-inner">
+                                                <img
+                                                    src="assets/icons/ping-meter.svg"
+                                                    className="sidebar-submenu-inner"
+                                                    alt="Benchmarks"
+                                                />
+                                                <div className="sidebar-head sidebar-inner">
+                                                    Ping
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                    <Link
+                                        to="/floodping"
+                                        style={{ textDecoration: "none" }}
+                                    >
+                                        <div>
+                                            <div className="sidebar-inner">
+                                                {/* <img
+                                                    src="assets/icons/ping-meter.svg"
+                                                    className="sidebar-submenu-inner"
+                                                    alt="Benchmarks"
+                                                /> */}
+                                                <div className="sidebar-head sidebar-inner">
+                                                    FloodPing
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                    <Link
+                                        to="/jitter"
+                                        style={{ textDecoration: "none" }}
+                                    >
+                                        <div>
+                                            <div className="sidebar-inner">
+                                                {/* <img
+                                                    src="assets/icons/bench-icon.svg"
+                                                    className="sidebar-submenu-inner"
+                                                    alt="Benchmarks"
+                                                /> */}
+                                                <div className="sidebar-head sidebar-inner">
+                                                    Jitter
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+                            ) : (
+                                <div />
+                            )}
+                        </div>
+
+                        <div className="sidebar-bottom-links">
+                            <Link
+                                to="/settings"
+                                style={{ textDecoration: "none" }}
+                            >
+                                <div>
+                                    <div className="sidebar-inner">
+                                        <img
+                                            src="assets/icons/settings-icon.svg"
+                                            className="sidebar-inner"
+                                            alt="settings"
+                                        />
+                                        <div className="sidebar-head sidebar-inner">
+                                            Settings
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </Link>
-            <Link to="/monitoring" style={{ textDecoration: "none" }}>
-              <div>
-                <div className="sidebar-inner">
-                  <img
-                    src="assets/icons/monitoring-icon.svg"
-                    className="sidebar-inner"
-                    alt="monitoring"
-                  />
-                  <div className="sidebar-head sidebar-inner">Monitoring</div>
-                </div>
-              </div>
-            </Link>
-            <Link to="/benchmarks" style={{ textDecoration: "none" }}>
-              <div>
-                <div className="sidebar-inner">
-                  <img
-                    src="assets/icons/bench-icon.svg"
-                    className="sidebar-inner"
-                    alt="Benchmarks"
-                  />
-                  <div className="sidebar-head sidebar-inner">Benchmarks</div>
-                </div>
-              </div>
-            </Link>
-            <div className="sidebar-bottom-links">
-            <Link to="/settings" style={{ textDecoration: "none" }}>
-              <div>
-                <div className="sidebar-inner">
-                  <img
-                    src="assets/icons/settings-icon.svg"
-                    className="sidebar-inner"
-                    alt="settings"
-                  />
-                  <div className="sidebar-head sidebar-inner">Settings</div>
-                </div>
-              </div>
-            </Link>
-          </div>
-          </div>
-        </div>
-      </Router>
-    );
-  }
+            </Router>
+        );
+    }
 }
