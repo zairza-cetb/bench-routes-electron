d281ad8db7ba6f420f0de2f5cd56526dbc4c17b2
muskankhedia <muskan.khedia2000@gmail.com>
2019-10-27 01:23:20 +0530

    fixing linting error
    Signed-off-by: muskankhedia <muskan.khedia2000@gmail.com>

--- src/components/benchmarks/Jitter.tsx
+++ src/components/benchmarks/Jitter.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class Jitter extends Component {
-    render() {
-        return <div>This is the Jitter Page.</div>;
-    }
+  render() {
+    return <div>This is the Jitter Page.</div>;
+  }
 }
