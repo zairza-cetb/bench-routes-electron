227f335bf7b40d6541724b6d0943d7fba897cf25
muskankhedia <muskan.khedia2000@gmail.com>
2019-10-27 00:58:55 +0530

    tslint fixes

--- src/router/Navigation.js
+++ src/router/Navigation.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "../components/dashboard/Dashboard";
-import Monitoring from "../components/monitoring/Monitoring";
-import Ping from "../components/benchmarks/Ping";
-import FloodPing from "../components/benchmarks/FloodPing";
-import Jitter from "../components/benchmarks/Jitter";
-import Settings from "../components/settings/Settings";
+import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from '../components/dashboard/Dashboard';
+import Monitoring from '../components/monitoring/Monitoring';
+import Ping from '../components/benchmarks/Ping';
+import FloodPing from '../components/benchmarks/FloodPing';
+import Jitter from '../components/benchmarks/Jitter';
+import Settings from '../components/settings/Settings';
 
 const Navigator = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/monitoring" component={Monitoring} />
-            <Route path="/ping" component={Ping} />
-            <Route path="/floodping" component={FloodPing} />
-            <Route path="/jitter" component={Jitter} />
-            <Route path="/settings" component={Settings} />
-        </Switch>
-    </Router>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/monitoring" component={Monitoring} />
+      <Route path="/ping" component={Ping} />
+      <Route path="/floodping" component={FloodPing} />
+      <Route path="/jitter" component={Jitter} />
+      <Route path="/settings" component={Settings} />
+    </Switch>
+  </Router>
 );
 
 export default Navigator;
